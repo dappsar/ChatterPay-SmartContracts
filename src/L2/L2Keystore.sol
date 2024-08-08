766f5a1c488c357ace73afdc8704a8ec0800812a
Tomas <tomasfreire@gmail.com>
2024-08-08 17:02:25 -0300

    L2Keystore format and style

--- src/L2/L2Keystore.sol
+++ src/L2/L2Keystore.sol
@@ -2,8 +2,16 @@
 
 pragma solidity ^0.8.24;
 
+/*//////////////////////////////////////////////////////////////
+                            IMPORTS
+//////////////////////////////////////////////////////////////*/
+
 import {IL1Keystore} from "../Ethereum/L1Keystore.sol";
 import {console} from "forge-std/console.sol";
+
+/*//////////////////////////////////////////////////////////////
+                            INTERFACES
+//////////////////////////////////////////////////////////////*/
 
 interface IL2Messenger {
   function sendMessage(address target, uint256 gasLimit, bytes calldata message) external;
@@ -17,7 +25,15 @@ interface IL2Keystore {
   function l1SloadGetWalletOwner(address wallet) external view returns (address);
 }
 
+/*//////////////////////////////////////////////////////////////
+                            CONTRACT
+//////////////////////////////////////////////////////////////*/
+
 contract L2Keystore {
+
+  /*//////////////////////////////////////////////////////////////
+                          STATE VARIABLES
+  //////////////////////////////////////////////////////////////*/
 
   address immutable l1Keystore;
   address immutable l2Messenger;
@@ -27,24 +43,35 @@ contract L2Keystore {
   bytes32 constant _UPDATE_KEY_GAS_LIMIT = keccak256("_UPDATE_KEY_GAS_LIMIT");
   bytes32 constant _WALLET_VERSION_KEY = keccak256("_WALLET_VERSION_KEY");
   bytes32 constant _WALLET_REGISTRY_SLOT = keccak256("_WALLET_REGISTRY_SLOT");
+
+  /*//////////////////////////////////////////////////////////////
+                              MODIFIERS
+  //////////////////////////////////////////////////////////////*/
     
   modifier canWrite(address account) {
     require(msg.sender == account);
     _;
   }
 
+  /*//////////////////////////////////////////////////////////////
+                              EVENTS
+  //////////////////////////////////////////////////////////////*/
+
   event WriteKey(address indexed account, bytes32 indexed key, bytes32 indexed value);
   event UpdateKey(address indexed account, bytes32 indexed key, bytes32 oldValue, bytes32 indexed newValue);
+
+  /*//////////////////////////////////////////////////////////////
+                              FUNCTIONS
+  //////////////////////////////////////////////////////////////*/
 
   constructor(address _l1Keystore, address _l2Messenger) {
     l1Keystore = _l1Keystore;
     l2Messenger = _l2Messenger;
   }
-  
-  // function loadKey(address account, bytes32 key) public returns (address) {
-  //   bytes32 slot = _computeKeySlot(account, key);
-  //   return l1Sload(l1Keystore, slot);
-  // }
+
+  /*//////////////////////////////////////////////////////////////
+                          PUBLIC FUNCTIONS
+  //////////////////////////////////////////////////////////////*/
 
   function l1SloadGetWalletOwner(address wallet) public view returns (address) {
     uint256 slot = _computeOwnerSlot(wallet);
@@ -58,30 +85,14 @@ contract L2Keystore {
     return abi.decode(ret, (address));
   }
 
-  // function writeKey(address account, bytes32 key, bytes32 value) public canWrite(account) {
-  //   require(loadKey(account, key) == address(0));
-  //   bytes memory _message = abi.encodeWithSignature("writeKey(address,bytes32,bytes32)", account, key, value);
-  //   // l2Messenger.sendMessage(l1Keystore, 0, _message, _WRITE_KEY_GAS_LIMIT);
-  //   emit WriteKey(account, key, value);
-  // }
-  
-  // function updateKey(address account, bytes32 key, bytes32 oldValue, bytes32 newValue) public canWrite(account) {
-  //   require(key != _SALT_KEY);
-  //   require(loadKey(account, key) == oldValue);
-  //   bytes memory _message = abi.encodeWithSignature("updateKey(address,bytes32,bytes32,bytes32)", account, key, oldValue, newValue);
-  //   IL2Messenger(l2Messenger).sendMessage(l1Keystore, 0, _message);
-  //   emit UpdateKey(account, key, oldValue, newValue);
-  // }
-  
-  // function getWalletImplementation(address account) public returns (address) {
-  //   bytes32 walletVersion; // = keyStorage[account][_WALLET_VERSION_KEY];
-  //   bytes32 slot = keccak256(abi.encodePacked(walletVersion, block.chainid, _WALLET_REGISTRY_SLOT));
-  //   address impl = address(bytes20(l1Sload(l1Keystore, slot)));
-  //   require(impl != address(0), "implementation is not registered");
-  //   return impl;
-  // }
+  /*//////////////////////////////////////////////////////////////
+                      VIEW AND PURE FUNCTIONS
+  //////////////////////////////////////////////////////////////*/
 
-  function _computeKeySlot(address account, bytes32 key) internal view returns (bytes32) {}
+  function getlatestL1BlockNumber() public view returns (uint256) {
+      uint256 l1BlockNum = IL1Blocks(L1_BLOCKS_ADDRESS).latestBlockNumber();
+      return l1BlockNum;
+  }
 
   function _computeOwnerSlot(address wallet) public pure returns (uint256) { // chante to internal after tests
     uint256 slotOfMapping = 0; // Slot of the mapping accounts
@@ -89,8 +100,34 @@ contract L2Keystore {
     return uint256(mappingSlot);
   }
 
-  function latestL1BlockNumber() public view returns (uint256) {
-      uint256 l1BlockNum = IL1Blocks(L1_BLOCKS_ADDRESS).latestBlockNumber();
-      return l1BlockNum;
-  }
+  function _computeKeySlot(address account, bytes32 key) internal pure returns (bytes32) {}
 }
+
+
+// function loadKey(address account, bytes32 key) public returns (address) {
+//   bytes32 slot = _computeKeySlot(account, key);
+//   return l1Sload(l1Keystore, slot);
+// }
+
+// function writeKey(address account, bytes32 key, bytes32 value) public canWrite(account) {
+//   require(loadKey(account, key) == address(0));
+//   bytes memory _message = abi.encodeWithSignature("writeKey(address,bytes32,bytes32)", account, key, value);
+//   // l2Messenger.sendMessage(l1Keystore, 0, _message, _WRITE_KEY_GAS_LIMIT);
+//   emit WriteKey(account, key, value);
+// }
+
+// function updateKey(address account, bytes32 key, bytes32 oldValue, bytes32 newValue) public canWrite(account) {
+//   require(key != _SALT_KEY);
+//   require(loadKey(account, key) == oldValue);
+//   bytes memory _message = abi.encodeWithSignature("updateKey(address,bytes32,bytes32,bytes32)", account, key, oldValue, newValue);
+//   IL2Messenger(l2Messenger).sendMessage(l1Keystore, 0, _message);
+//   emit UpdateKey(account, key, oldValue, newValue);
+// }
+
+// function getWalletImplementation(address account) public returns (address) {
+//   bytes32 walletVersion; // = keyStorage[account][_WALLET_VERSION_KEY];
+//   bytes32 slot = keccak256(abi.encodePacked(walletVersion, block.chainid, _WALLET_REGISTRY_SLOT));
+//   address impl = address(bytes20(l1Sload(l1Keystore, slot)));
+//   require(impl != address(0), "implementation is not registered");
+//   return impl;
+// }
