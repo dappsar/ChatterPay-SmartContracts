1d3f2964c686dce4cfc23308e3f2cff02ac859c8
Tomas Di Mauro <info@tmdm.com.ar>
2024-10-29 18:29:37 -0300

    [fix] support mints to proxies

--- src/L2/ChatterPayNFT.sol
+++ src/L2/ChatterPayNFT.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import {ERC721URIStorageUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-
 
 error ChatterPayNFT__Unauthorized();
 error ChatterPayNFT__TokenAlreadyMinted(uint256);
@@ -16,20 +14,10 @@ error ChatterPayNFT__LimitExceedsCopies();
 contract ChatterPayNFT is UUPSUpgradeable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable {
     
     uint256 private s_tokenId;
-    mapping (address => bool) public s_authorized;
     mapping (uint256 tokenId => address minter) public s_originalMinter;
     mapping(uint256 tokenId => uint256 copies) public s_copyCount;
     mapping(uint256 tokenId => uint256 copyLimit) public s_copyLimit;
     string private s_baseURI;
-
-    event Authorized(address indexed user, bool indexed value);
-
-    modifier onlyOwnerOrAuthorized() {
-        if(msg.sender != owner() && !s_authorized[msg.sender]) {
-            revert ChatterPayNFT__Unauthorized();
-        }
-        _;
-    }
 
     function initialize(address initialOwner, string memory baseURI) public initializer {
         __ERC721_init("ChatterPayNFT", "CHTP");
@@ -44,29 +32,25 @@ contract ChatterPayNFT is UUPSUpgradeable, ERC721Upgradeable, ERC721URIStorageUp
         return s_baseURI;
     }
 
-    function mintOriginal(address to, string memory uri) public onlyOwnerOrAuthorized {
-        s_tokenId++; // index 1
-        if(s_tokenId % 10 == 0) s_tokenId++; // never ending in 0 to avoid conflicts with copies
+    function mintOriginal(address to, string memory uri) public {
+        s_tokenId++;
+        if(s_tokenId % 10 == 0) s_tokenId++;
         uint256 tokenId = s_tokenId;
         s_copyLimit[tokenId] = 1000; // default limit
-        if(s_originalMinter[tokenId] != address(0)) revert ChatterPayNFT__TokenAlreadyMinted(tokenId);
-        s_originalMinter[tokenId] = to;
-        _safeMint(to, tokenId);
+        // The msg.sender (who pays for the gas) is the original minter
+        s_originalMinter[tokenId] = msg.sender;
+        // The NFT goes to the recipient
+        _mint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-    function mintCopy(address to, uint256 originalTokenId, string memory uri) public onlyOwnerOrAuthorized {
+    function mintCopy(address to, uint256 originalTokenId, string memory uri) public {
         if(s_originalMinter[originalTokenId] == address(0)) revert ChatterPayNFT__OriginalTokenNotMinted(originalTokenId);
         if(s_copyCount[originalTokenId] >= s_copyLimit[originalTokenId]) revert ChatterPayNFT__LimitExceedsCopies();
         s_copyCount[originalTokenId]++;
         uint256 copyTokenId = originalTokenId * 10**8 + s_copyCount[originalTokenId];
-        _safeMint(to, copyTokenId);
+        _mint(to, copyTokenId);
         _setTokenURI(copyTokenId, uri);
-    }
-
-    function setAuthorized(address user, bool value) public onlyOwner {
-        s_authorized[user] = value;
-        emit Authorized(user, value);
     }
 
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
@@ -80,7 +64,6 @@ contract ChatterPayNFT is UUPSUpgradeable, ERC721Upgradeable, ERC721URIStorageUp
     }
 
     // The following functions are overrides required by Solidity.
-
     function tokenURI(uint256 tokenId)
         public
         view
